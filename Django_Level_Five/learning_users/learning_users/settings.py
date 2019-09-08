f89d3606fd494c90c3af617c7f24b465a67f7036
zhangboz <38872861+zhangboz@users.noreply.github.com>
2019-09-07 17:26:46 -0700

    allowed hosts

--- Django_Level_Five/learning_users/learning_users/settings.py
+++ Django_Level_Five/learning_users/learning_users/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'q2lra#ddiscy17p3t-&^m1+ic#xba!6zkyfe-&*d#+vh!%q*ev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zhangboz.pythonanywhere.com',]
 
 
 # Application definition
